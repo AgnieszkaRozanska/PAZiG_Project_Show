86d19f677f298e31911c02234a64ba85d97e94f6
Agnieszka Różańska <agniroz767@student.polsl.pl>
2023-03-19 12:15:07 +0100

    Create interface IOperations and class Operations, implementation Add function

---
+++ Calculator/Services/Operations.cs
@@ -0,0 +1,17 @@
+﻿using Calculator.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Services
+{
+    internal class Operations : IOperations
+    {
+        public double Add(double firstValue, double secondValue)
+        {
+            return firstValue + secondValue;
+        }
+    }
+}
